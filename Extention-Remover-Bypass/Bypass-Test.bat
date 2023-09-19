637d598aa8720a7b7e9e45687333279a0da3ea57
impresson <129333315+impresson@users.noreply.github.com>
2023-09-19 12:01:20 +1200

    Extention Remover Bypass
    Added vPR-0.1

---
+++ Extention-Remover-Bypass/Bypass-Test.bat
@@ -0,0 +1,16 @@
+title Extention Remover Bypass
+echo off
+cls
+echo if you start please finish this is your last time to leave ;D
+pause
+ipconfig /release
+cls
+start chrome.exe
+echo Please continue once you selected your profile and your chrome has FULLY loaded!
+pause 
+ipconfig /renew
+cls
+echo your extention has been disabled safely
+echo enjoy ;D
+pause
+exit
