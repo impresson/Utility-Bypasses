9e72db3e05e9a75a5b544ccdef031f0ab1239f12
impresson <129333315+impresson@users.noreply.github.com>
2023-09-20 12:39:12 +1200

    Added Extension Remover
    I was too lazy to fix spelling

---
+++ Extension-Remover-Bypass/Patcher.bat
@@ -0,0 +1,8 @@
+title patcher
+echo off
+cls
+ipconfig /renew
+cls
+del "C:\Users\(INSERT USER HERE)\AppData\Local\Google\Chrome\User Data\(YOUR CHROME PROFILE)\Extensions\(YOUR EXTENTION PATH HERE)\"
+copy "C:\Users\(INSERT USER HERE)\downloads\Extention-Remover-Bypass\PR-0.1" "C:\Users\(INSERT USER HERE)\AppData\Local\Google\Chrome\User Data\(YOUR CHROME PROFILE)\Extensions\(EXTENTION PATH HERE)\"
+pause
